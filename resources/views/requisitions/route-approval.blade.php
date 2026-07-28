2e8362554c4bbe98495cf30a3ba7135ea98285ad
shilbyanne <shilbyanne@gmail.com>
2026-07-28 01:58:02 -0700

    Remove route-approval.blade.php from PR

--- resources/views/requisitions/route-approval.blade.php
+++ resources/views/requisitions/route-approval.blade.php
@@ -166,8 +166,8 @@
             approversFor(stepType) {
                 const roleMap = {
                     manager_approval: 'manager',
-                    finance_approval: 'finance_manager',
                     department_head_approval: 'department_head',
+                    finance_approval: 'finance_manager',
                 };
                 return this.users.filter(u => u.role === roleMap[stepType]);
             },
